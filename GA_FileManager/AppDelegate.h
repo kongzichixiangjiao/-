bc7378bfe2aefdedaffa9728d8d0a985b5abcea3
kongzichixiangjiao <58895347@qq.com>
2016-01-30 22:36:49 +0800

    Initial Commit

---
+++ GA_FileManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GA_FileManager
+//
+//  Created by houjianan on 16/1/30.
+//  Copyright © 2016年 houjianan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GA_FileManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GA_FileManager
+//
+//  Created by houjianan on 16/1/30.
+//  Copyright © 2016年 houjianan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
